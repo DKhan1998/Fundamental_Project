98ccbb662814d6ff3ce66150bbc08911b9f459c8
Daood Khan <DKhan@qa.com>
2020-09-11 09:14:51 +0100

    config file - Adjusted clone url
    Signed-off-by: Daood Khan <DKhan@qa.com>

--- src/main/java/com/qa/billyshakes/BillyshakesApplication.java
+++ src/main/java/com/qa/billyshakes/BillyshakesApplication.java
@@ -8,7 +8,6 @@ public class BillyshakesApplication {
 
     public static void main(String[] args) {
        SpringApplication.run(BillyshakesApplication.class, args);
-
     }
 
 }
--- src/main/java/com/qa/billyshakes/repo/OrderLIneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qa.billyshakes.repo;
-
-import com.qa.billyshakes.domain.OrderLine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderLIneRepository extends JpaRepository<OrderLine, Long> {
-}
